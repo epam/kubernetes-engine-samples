f3d46ef8ae22a5ae51eb1745b45012f20eb65e64
Kiryl <kiryl_filatau@epam.com>
2024-12-05 17:11:07 +0100

    test with autopilot

--- ai-ml/hyperdisk/run-commands.sh
+++ ai-ml/hyperdisk/run-commands.sh
@@ -48,15 +48,15 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 # Run the Cloud Build command to prepare the cluster with all required substitutions
 gcloud builds submit \
   --config cloudbuild-prepare-autopilot.yaml --no-source \
-  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID,_CLUSTER_TYPE=autopilot
+  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID
 
 gcloud builds submit \
   --config cloudbuild-prepare-standard.yaml --no-source \
-  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_ZONE_A="$ZONE_A",_ZONE_B="$ZONE_B",_ZONE_C="$ZONE_C",_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID,_CLUSTER_TYPE-standard
+  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_ZONE_A="$ZONE_A",_ZONE_B="$ZONE_B",_ZONE_C="$ZONE_C",_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID
 
 gcloud builds submit \
   --config cloudbuild-preload-apply.yaml --no-source \
-  --substitutions=_REGION=$REGION,_CLUSTER_NAME=$CLUSTER_NAME,_HF_TOKEN=$HF_TOKEN
+  --substitutions=_REGION=$REGION,_CLUSTER_NAME=$CLUSTER_NAME,_HF_TOKEN=$HF_TOKEN,_CLUSTER_TYPE=standard
 
 # Check the logs of the pod
 kubectl logs $(kubectl get pods -o jsonpath='{.items[0].metadata.name}')
