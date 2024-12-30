5a4ef63e38a724be35b88cc3b38b0c7e27fb5f80
Kiryl <kiryl_filatau@epam.com>
2024-12-30 20:46:58 +0100

    add autopilot fix

--- ai-ml/hyperdisk/run-commands.sh
+++ ai-ml/hyperdisk/run-commands.sh
@@ -26,6 +26,10 @@ export PROJECT_ID=$(gcloud config get project) \
 && export HF_TOKEN=HF_TOKEN \
 && for zone in A B C … ; do export ZONE_$zone="$REGION-$(echo $zone | tr A-Z a-z)"; done
 
+
+echo -n ${HF_USERNAME} | gcloud secrets create hf-username --data-file=- \
+&& echo -n ${HF_TOKEN} | gcloud secrets create hf-token --data-file=-
+
 # Add the required permissions to the default Cloud Build service account
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
@@ -45,15 +49,17 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
     --condition=None
 
 
-# Run the Cloud Build command to prepare the cluster with all required substitutions
+# Run the Cloud Build command to prepare the AUTOPILOT cluster with all required substitutions
 gcloud builds submit \
   --config cloudbuild-prepare-autopilot.yaml --no-source \
-  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID
+  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_ZONE_A=$ZONE_A,_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID
 
+# Run the Cloud Build command to prepare the STANDARD cluster with all required substitutions
 gcloud builds submit \
   --config cloudbuild-prepare-standard.yaml --no-source \
-  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_ZONE_A="$ZONE_A",_ZONE_B="$ZONE_B",_ZONE_C="$ZONE_C",_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID
+  --substitutions=_DISK_IMAGE=$DISK_IMAGE,_CONTAINER_IMAGE=$CONTAINER_IMAGE,_BUCKET_NAME=$LOG_BUCKET_NAME,_REGION=$REGION,_ZONE_A=$ZONE_A,_ZONE_B=$ZONE_B,_ZONE_C=$ZONE_C,_CLUSTER_NAME=$CLUSTER_NAME,_PROJECT_ID=$PROJECT_ID
 
+# Run the Cloud Build command to preload model files to the HDML and apply all the required manifests.
 gcloud builds submit \
   --config cloudbuild-preload-apply.yaml --no-source \
   --substitutions=_REGION=$REGION,_CLUSTER_NAME=$CLUSTER_NAME,_HF_TOKEN=$HF_TOKEN,_CLUSTER_TYPE=standard
@@ -64,4 +70,22 @@ kubectl logs $(kubectl get pods -o jsonpath='{.items[0].metadata.name}')
 # Clean-up
 gcloud builds submit \
   --config cloudbuild-cleanup.yaml --no-source \
-  --substitutions=_CLUSTER_NAME=$CLUSTER_NAME,_REGION=$REGION,_PROJECT_ID=$PROJECT_ID,_PROJECT_NUMBER=$PPROJECT_NUMBER,_BUCKET_NAME=$LOG_BUCKET_NAME,_DISK_IMAGE=$DISK_IMAGE
+  --substitutions=_CLUSTER_NAME=$CLUSTER_NAME,_REGION=$REGION,_BUCKET_NAME=$LOG_BUCKET_NAME,_DISK_IMAGE=$DISK_IMAGE
+
+# Remove permisions
+gcloud projects remove-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
+    --role="roles/storage.admin" \
+    --condition=None \
+&& gcloud projects remove-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
+    --role="roles/secretmanager.secretAccessor" \
+    --condition=None \
+&& gcloud projects remove-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
+    --role="roles/container.clusterAdmin" \
+    --condition=None \
+&& gcloud projects remove-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
+    --role="roles/compute.instanceAdmin.v1" \
+    --condition=None
